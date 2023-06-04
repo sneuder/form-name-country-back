a1d23390f59a90e6d2475f8d97d6e1ab443e94c4
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-06-04 18:29:13 -0500

    schema: country

--- src/country/dto/country.output.ts
+++ src/country/dto/country.output.ts
@@ -4,4 +4,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 export class CountryOutput {
   @Field(() => String, { nullable: true })
   name: string;
+
+  @Field(() => String, { nullable: true })
+  country: string;
 }
